8bfd1c6df8326fa8ad45ad834c70137fef56f83e
OllieJC <5426038+OllieJC@users.noreply.github.com>
2025-06-01 22:19:14 +0100

    Initial flask app

---
+++ flask-app/app.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import os
+from flask import Flask, redirect, url_for, session, render_template
+from authlib.integrations.flask_client import OAuth
+
+app = Flask(__name__, template_folder="templates", static_folder="assets")
+app.secret_key = os.getenv("SECRET_KEY", os.urandom(24))
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+
+oauth = OAuth(app)
+oauth.register(
+    name="internal_access",
+    server_metadata_url="https://sso.service.security.gov.uk/.well-known/openid-configuration",
+    client_id=CLIENT_ID,
+    client_secret=os.getenv("CLIENT_SECRET"),
+    client_kwargs={"scope": "openid profile email"},
+)
+
+
+@app.route("/")
+def route_root():
+    user = session.get("user")
+    if user:
+        return render_template(
+            "root.html",
+            user=user,
+            navigation=[
+                {"label": "Home", "url": url_for("route_root"), "active": True},
+                {"label": "Logout", "url": url_for("route_logout")},
+            ],
+        )
+    return redirect(url_for("route_login"))
+
+
+@app.route("/login")
+def route_login():
+    redirect_uri = url_for("route_auth", _external=True)
+    session["nonce"] = os.urandom(16).hex()
+    return oauth.internal_access.authorize_redirect(
+        redirect_uri, nonce=session["nonce"]
+    )
+
+
+@app.route("/logout")
+def route_logout():
+    session.pop("user", None)
+    return redirect(
+        f"https://sso.service.security.gov.uk/sign-out?from_app={CLIENT_ID}"
+    )
+
+
+@app.route("/auth")
+def route_auth():
+    token = oauth.internal_access.authorize_access_token()
+    userinfo = oauth.internal_access.parse_id_token(token, nonce=session.get("nonce"))
+    session["user"] = userinfo
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=int(os.getenv("PORT", 5015)), debug=True)
---
+++ flask-app/readme.md
@@ -0,0 +1,35 @@
+# Python Flask App
+
+Start by using a virtual environment:
+
+``` bash
+python -m venv .venv
+```
+
+Activate virtual environment using:
+
+``` bash
+# Windows (PowerShell)
+.\.venv\Scripts\activate.ps1
+
+# macOS/Linux
+source .venv/bin/activate
+```
+
+Install dependencies:
+
+``` bash
+python -m pip install -r requirements.txt
+```
+
+Configure the `run.ps1` or `run.sh` file and run: 
+
+``` bash
+# Windows (PowerShell)
+run.ps1
+
+# macOS/Linux
+run.sh
+```
+
+Open using <http://localhost:5015>!
