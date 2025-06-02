c798a384f615ae409f394000b2a77e64f17c9346
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2025-06-02 12:27:00 +0200

    hit API

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,6 +4,7 @@ import os
 from flask import Flask, redirect, url_for, session, render_template
 from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv
+from grist_api import GristDocAPI
 
 load_dotenv()  # take environment variables
 
@@ -11,6 +12,11 @@ app = Flask(__name__, template_folder="templates", static_folder="assets")
 app.secret_key = os.getenv("SECRET_KEY", os.urandom(24))
 
 CLIENT_ID = os.getenv("CLIENT_ID")
+
+GRIST_DOC_ID = os.getenv("GRIST_DOCUMENT_ID")
+GRIST_SERVER = os.getenv("GRIST_SERVER")
+
+grist = GristDocAPI(GRIST_DOC_ID, server=GRIST_SERVER)
 
 oauth = OAuth(app)
 oauth.register(
@@ -61,6 +67,10 @@ def route_auth():
     session["user"] = userinfo
     return redirect("/")
 
+@app.route("/lists")
+def route_lists():
+    lists = grist.fetch_table("GroupMetadata")
+    return lists
 
 if __name__ == "__main__":
     app.run(host="localhost", port=int(os.getenv("PORT", 5015)), debug=True)
